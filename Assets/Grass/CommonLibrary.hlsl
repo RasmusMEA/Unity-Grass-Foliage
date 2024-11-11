366e3ee9bc4bb46ac1c329ecf21220503277612d
Rasmus Andersson <70899867+RasmusMEA@users.noreply.github.com>
2024-11-12 00:48:04 +0100

    Added another random function

--- Assets/Grass/CommonLibrary.hlsl
+++ Assets/Grass/CommonLibrary.hlsl
@@ -41,4 +41,43 @@ float3 UniformRandomBarycentricCoefficients(float seedA, float seedB) {
     return float3(1 - r1 - r2, r1, r2);
 }
 
+struct RandomResult
+{
+    uint4 state;
+    float value;
+};
+
+uint TausStep(uint z, int S1, int S2, int S3, uint M)
+{
+    uint b = (((z << S1) ^ z) >> S2);
+    return (((z & M) << S3) ^ b);    
+}
+
+uint LCGStep(uint z, uint A, uint C)
+{
+    return (A * z + C);    
+}
+
+RandomResult Random(uint4 state)
+{
+    state.x = TausStep(state.x, 13, 19, 12, 4294967294);
+    state.y = TausStep(state.y, 2, 25, 4, 4294967288);
+    state.z = TausStep(state.z, 3, 11, 17, 4294967280);
+    state.w = LCGStep(state.w, 1664525, 1013904223);
+
+    RandomResult result;
+    result.state = state;
+    result.value = 2.3283064365387e-10 * (state.x ^ state.y ^ state.z ^ state.w);
+
+    return result;
+}
+
+float3 BarycentricCoefficients(float r1, float r2) {
+    if (r1 + r2 > 1) {
+        r1 = 1 - r1;
+        r2 = 1 - r2;
+    }
+    return float3(1 - r1 - r2, r1, r2);
+}
+
 #endif // COMMONLIBRARY_HLSL
